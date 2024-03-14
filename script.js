58de81be3f720adc606837922d059f6a5904200f
saraazizhere <saraaziz180903@gmail.com>
2024-03-15 01:10:18 +0500

    Final Commit

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-let runningTotal = 0;
-let buffer = "0";
+let currentOperand = "0";
+let previousOperand = "";
+let operation = null;
+let equals = null;
 const buttons = document.querySelectorAll("button");
-const previousOperand = document.querySelector(".previous-operand");
-const currentOperand = document.querySelector(".current-operand");
-
-buttons.forEach(button => {
-    button.addEventListener("click", function(event) {
-        handleButtonClick(event.target.alt);
-    })
-});
+const previousOperandTextElement = document.querySelector(".previous-operand");
+const currentOperandTextElement = document.querySelector(".current-operand");
 
 function handleButtonClick(value) {
     if (isNaN(parseInt(value))) {
@@ -22,35 +18,106 @@ function handleButtonClick(value) {
 function handleSymbol(value) {
     switch(value) {
         case "C":
-            runningTotal = 0;
-            buffer = "0";
+            previousOperand = "";
+            currentOperand = "0";
+            operation = null;
+            equals = null;
             break;
         case "←":
-            buffer = buffer.substring(0, buffer.length - 1);
+            if (currentOperand.length === 1) {
+                currentOperand = "0";
+            } else {
+                currentOperand = currentOperand.substring(0, currentOperand.length - 1);
+            }
+            break;
+        case ".":
+            if (currentOperand.includes(".")) break;
+            currentOperand += value;
+            break;
+        case "=":
+            equals = "=";
+            compute();
+            updateScreen();
             break;
         case "+":
-
+            chooseOperation("+");
+            break;
         case "-":
-
+            chooseOperation("-");
+            break;
         case "*":
-
-        case "/":
-
-        case ".":
-
-        case "=":
-
+            chooseOperation("*");
+            break;
+        case "÷":
+            chooseOperation("÷");
+            break;
     }
 };
 
 function handleNumber(value) {
-    if (buffer === "0") {
-        buffer = value;
-    } else {
-        buffer += value;
+    if (currentOperand === "0" || equals === "=") {
+        currentOperand = value;
+        equals = null;
     }
+    else {
+        currentOperand += value;
+    }
+};
+
+function chooseOperation(currentOperation) {
+    if (currentOperand === "") return;  // if no number entered, and an operation entered, do nothing and return
+    if (previousOperand !== "") {
+        compute();
+    }
+    operation = currentOperation;
+    previousOperand = currentOperand;
+    currentOperand = "";
+};
+
+function compute() {
+    let computation;
+    const previous = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+
+    if (isNaN(previous) || isNaN(current)) return;
+
+    switch(operation) {
+        case "+":
+            computation = previous + current;
+            break;
+        case "-":
+            computation = previous - current;
+            break;
+        case "*":
+            computation = previous * current;
+            break;
+        case "÷":
+            computation = previous / current;
+            break;
+    }
+
+    currentOperand = computation;
+    previousOperand = "";
+    operation = null;
 }
 
 function updateScreen() {
-    currentOperand.innerText = buffer;
-}
+    currentOperandTextElement.innerText = currentOperand;
+    if (operation !== null) {
+        previousOperandTextElement.innerText = previousOperand + operation;
+    } else {
+        previousOperandTextElement.innerText = "";
+    }
+};
+
+function init() {
+    updateScreen();
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            handleButtonClick(event.target.alt);
+        })
+    });
+};
+
+init();
